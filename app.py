8cc54570993dd56c2e5ba84ad8b7df283102c784
AlexKay28 <kaygorodow@mail.ru>
2021-09-22 22:43:27 +0300

    fixed time confusion bug

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
+class Session:
+    asked_first_language = None
+    asked_second_language = None
+
+
+s = Session()
+
+
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
@@ -13,21 +21,28 @@ def index():
 
 @app.route("/practice", methods=["POST", "GET"])
 def home():
-    first_language, second_language = get_languages_pair(
-        "english",
-        "french",
-        "level1",
-    )
-    second_language_answer = ""
     if request.method == "POST":
+        first_language = s.asked_first_language
+        second_language = s.asked_second_language
         second_language_answer = request.form["second_language_answer"]
-
-    return render_template(
-        "practice.html",
-        first_language=first_language,
-        second_language_answer=second_language_answer,
-        second_language=second_language,
-    )
+        return render_template(
+            "practice_answer.html",
+            first_language=first_language,
+            second_language=second_language,
+            second_language_answer=second_language_answer,
+        )
+    else:
+        first_language, second_language = get_languages_pair(
+            "english",
+            "french",
+            "level1",
+        )
+        s.asked_first_language = first_language
+        s.asked_second_language = second_language
+        return render_template(
+            "practice_ask.html",
+            first_language=first_language,
+        )
 
 
 if __name__ == "__main__":
