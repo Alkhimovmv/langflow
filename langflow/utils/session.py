9eed1d2ffe28afcdd2b760f7bae87bd42315a351
Alexander Kaigorodov <55444371+AlexKay28@users.noreply.github.com>
2021-10-06 22:50:39 +0300

    #33 auto untar (#34)
    * change proc file
    
    * add untar procedure
    
    * remove pkg-resources

--- langflow/utils/session.py
+++ langflow/utils/session.py
@@ -1,10 +1,28 @@
 import os
+import gzip
+import shutil
 import fasttext
 import numpy as np
 import pandas as pd
 
 PATH_TO_DATA = "data/phrases.csv"
 PATH_TO_MODELS = "language_models/"
+
+
+def extract_language_models(model_name, extension=".gz"):
+    """
+    Models are stored as .bit.tar.gz
+    Fore session working they are needed to be extracted
+    """
+    archive_path = os.path.join(PATH_TO_MODELS, f"{model_name}.gz")
+    model_path = os.path.join(PATH_TO_MODELS, f"{model_name}")
+
+    if model_name in os.listdir(PATH_TO_MODELS):
+        return
+
+    with gzip.open(archive_path, "rb") as file_in:
+        with open(model_path, "wb") as file_out:
+            shutil.copyfileobj(file_in, file_out)
 
 
 class SessionController:
@@ -16,9 +34,6 @@ class SessionController:
     def __init__(self, first_language, second_language, level):
         self.first_language = first_language
         self.second_language = second_language
-        self.language_model = fasttext.load_model(
-            os.path.join(PATH_TO_MODELS, f"{second_language}5.bin")
-        )
         self.level = level
         self.pairs = pd.read_csv(
             PATH_TO_DATA,
@@ -27,6 +42,12 @@ class SessionController:
         self.pairs = self.pairs[
             self.pairs.level.apply(lambda l: l == level if level > 0 else True)
         ][[self.first_language, self.second_language]].values
+
+        self.model_name = f"{second_language}5.bin"
+        extract_language_models(self.model_name)
+        self.language_model = fasttext.load_model(
+            os.path.join(PATH_TO_MODELS, self.model_name)
+        )
 
         # highly dynamic variables
         self.first_language_phrase = None
