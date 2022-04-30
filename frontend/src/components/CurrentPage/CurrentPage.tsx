9d9b029d2c9638789707331779cf37fa2c8b6b5e
Alkhimov Maxim <61086417+Alkhimovmv@users.noreply.github.com>
2022-04-30 10:21:20 +0300

    redesign answer card (#116)

--- frontend/src/components/CurrentPage/CurrentPage.tsx
+++ frontend/src/components/CurrentPage/CurrentPage.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import cn from 'classnames'
 
 import { Router, Switch, Route, Redirect } from 'react-router-dom'
 import Question from '../Question/Question'
@@ -11,25 +11,26 @@ import Footer from '../Footer/Footer'
 
 import { history } from '../helpers/history'
 
-class CurrentPage extends Component {
-    render() {
-        return (
-            <Router history={history}>
-                <div className="vh-100 text-center bg-light">
-                    <Header/>
-                    <Switch>
-                        <Route path='/home' component={HomePage} />
-                        <Route path='/ourteam' component={OurTeam} />
-                        <Route path='/question' component={Question} />
-                        <Route path='/registration' component={RegistrationPage} />
-                        <Route path='/login' component={LoginPage} />
-                        <Redirect from='/' to='/home'/>
-                    </Switch>
-                    <Footer/>
-                </div>
-            </Router>
-        );
-    }
-};
+const CurrentPage = () => {
+    const isQuestionPage = window.location.pathname === '/question'
+    const classes = cn({ 'vh-100': !isQuestionPage, 'min-vh-100': isQuestionPage }, 'text-center bg-light')
+
+    return (
+        <Router history={history}>
+            <div className={classes}>
+                <Header/>
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route path='/ourteam' component={OurTeam} />
+                    <Route path='/question' component={Question} />
+                    <Route path='/registration' component={RegistrationPage} />
+                    <Route path='/login' component={LoginPage} />
+                    <Redirect from='/' to='/home'/>
+                </Switch>
+                <Footer/>
+            </div>
+        </Router>
+    )
+}
 
 export default CurrentPage
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import './footer.scss'
 
+import cn from 'classnames'
+
 const Footer = (): JSX.Element => {
+    const isQuestionPage = window.location.pathname === '/question'
+    const classes = cn({ 'position-relative': isQuestionPage }, 'footer')
+
     return (
         <footer>
-            <div className='footer'>Service created by <a className='footer-link' href="https://github.com/AlexKay28">AlexKay</a> and <a className='footer-link' href="https://github.com/Alkhimovmv">AlkhimovMV</a>.
+            <div className={classes}>Service created by <a className='footer-link' href="https://github.com/AlexKay28">AlexKay</a> and <a className='footer-link' href="https://github.com/Alkhimovmv">AlkhimovMV</a>.
             </div>
         </footer>
     )
